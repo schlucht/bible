08b10f8b09f0c7bc47ef59ac7baee12502eeaead
schmidlothar1 <schmidlothar1@gmail.com>
2025-11-03 12:35:53 +0000

    ADD - New Projekt mit SLIM

--- public/index.php
+++ public/index.php
@@ -1,7 +1,30 @@
-
 <?php
 
-    require_once "src/bootstrap.php";
+declare(strict_types=1);
+
+use Ots\Bible\Database;
+use Slim\Factory\AppFactory;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Psr\Http\Message\ResponseInterface as Response;
    
+require dirname(__DIR__) . '/public/vendor/autoload.php';
+
+$app = AppFactory::create();
+
+$app->get('/api/users', function (Request $request, Response $response){
+
+    $database = new Database;
+    $pdo = $database->getConnection();
+    $stmt = $pdo->query("SELECT * FROM users");
     
+    $res = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+    $body = json_encode($res);
+
+    $response->getBody()->write($body);
+    return $response->withHeader('Content-Type', 'application/json');
+
+});
+
+$app->run();
 ?>
---
+++ public/src/Database.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ots\Bible;
+
+use PDO;
+
+class Database{
+    public function getConnection():PDO
+    {
+        $dsn = "mysql:host=db;dbname=schlucht;charset=utf8";
+
+        $pdo = new PDO($dsn, "schlucht", "schlucht");
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+        return $pdo;
+    }
+}
